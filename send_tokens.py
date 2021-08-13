98c8b33c4d12174a4ec4bd7ec422f1a7551b96fd
qianhuang219 <77247587+qianhuang219@users.noreply.github.com>
2021-08-13 18:07:51 -0500

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -32,7 +32,7 @@ def connect_to_algo(connection_type=''):
 
 
 def send_tokens_algo(acl, sender_sk, txes):
-    params = acl.suggested_params() #********************
+    params = acl.suggested_params #********************
 
     # TODO: You might want to adjust the first/last valid rounds in the suggested_params #**************
     #       See guide for details
